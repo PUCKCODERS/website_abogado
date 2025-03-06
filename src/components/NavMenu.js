7cd815ad929cfd64bcc57e60b7908b869b1a60b6
PUCKCODERS <jonitaweb.jobs@gmail.com>
2025-03-05 21:05:17 -0500

    menu terminado

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { MdMenu } from "react-icons/md";
@@ -11,7 +11,7 @@ const NavMenuStyles = styled.div`
   left: 0;
   width: 100%;
   padding: 1rem 0;
-  background-color: var(--deep-dark);
+  background: var(--dark-bg);
     ul {
     max-width: 1200px;
     width: 90%;
@@ -43,45 +43,102 @@ const NavMenuStyles = styled.div`
     cursor: pointer;
     display: none;
     outline: none;
+    *{
+    pointer-events: none;}
     }
     .closeNavIcon {
     display: none;
     }
     @media only screen and (max-width: 768px) {
+    padding: 0; 
+     .hide-item {
+     transform: translateY(calc(-100% - var(--top)));
+     }
      .mobile-menu-icon {
         display: block;
         }
      .navItems {
+        --top: 1rem;
         transition: 0.3s ease transform;
         background-color: var(--deep-dark);
-    }
-     .closeNavIcon {
+        padding: 2rem;
+        width: 90%;
+        max-width: 300px;
+        border-radius: 12px;
+        position: absolute;
+        right: 1rem;
+        top: var(--top);
+        .closeNavIcon {
         display: block;
+        width: 3rem;
+        margin: 0 0 0 auto;
+        cursor: pointer;
+        * {
+        pointer-events: none;
         }
+        }
+        li{
+        display: block;
+        margin-bottom: 1rem;
+        }
+    }
+     
     }
 `;
 
 export default function NavMenu() {
+    const[showNav, SetShowNav] = useState(false);
     return (
         <NavMenuStyles>
-            <div className="mobile-menu-icon">
+            <div className="mobile-menu-icon" 
+            onClick={() => SetShowNav(!showNav)}
+            role="button"
+            onKeyDown={() => SetShowNav(!showNav)}
+            tabIndex={0}
+            >
                 <MdMenu />
             </div>
-            <ul>
-                <div className="closeNavIcon">
+            <ul className={!showNav ? "navItems hide-item" : "navItems show"}>
+                <div 
+                className="closeNavIcon"
+                onClick={() => SetShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => SetShowNav(!showNav)}
+                tabIndex={0}
+                >
                     <MdClose />
                 </div>
                 <li>
-                <NavLink to="/" exact>INICIO</NavLink>
+                <NavLink to="/" exact
+                onClick={() => SetShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => SetShowNav(!showNav)}
+                tabIndex={0}
+                >INICIO</NavLink>
                 </li>
                 <li>
-                <NavLink to="/about">SOBRE MI</NavLink>
+                <NavLink to="/about"
+                onClick={() => SetShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => SetShowNav(!showNav)}
+                tabIndex={0}
+                >SOBRE MI</NavLink>
                 </li>
                 <li>
-                <NavLink to="/projects">ESPECIALIDADES</NavLink>
+                <NavLink to="/projects"
+                onClick={() => SetShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => SetShowNav(!showNav)}
+                tabIndex={0}
+                >ESPECIALIDADES</NavLink>
                 </li>
                 <li>
-                <NavLink to="/contact">CONTACTO</NavLink>
+                <NavLink to="/contact"
+                onClick={() => SetShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => SetShowNav(!showNav)}
+                tabIndex={0}
+                >CONTACTO</NavLink>
                 </li>
                 
             </ul>

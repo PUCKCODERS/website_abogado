a30d5f35bd958e8252610389f1d60dd0510eb392
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-01-21 22:01:31 -0500

    Actualizacion

--- src/components/PText.js
+++ src/components/PText.js
@@ -2,19 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const PStyle = styled.div`
-max-width:500px;
-margin: 0 auto;
-font-size: 1.7rem;
-line-height: 2.5rem;
-@media only screen and (max-width: 768px){
-font-size: 1.4rem;
-}
+  max-width: 500px;
+  margin: 0 auto;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+  word-break: break-all;
+  overflow-wrap: break-word;
+  @media only screen and (max-width: 768px) {
+    font-size: 1.4rem;
+  }
 `;
 
-export default function PText({children}) {
-    return (
-        <PStyle className="para">
-            <p>{children}</p>
-        </PStyle>
-    );
+export default function PText({ children }) {
+  return (
+    <PStyle className="para">
+      <p>{children}</p>
+    </PStyle>
+  );
 }

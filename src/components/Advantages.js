7ccc879ec4617e29bba04d0de370285136fa098c
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-01-21 21:13:04 -0500

    Actualizacion

--- src/components/Advantages.js
+++ src/components/Advantages.js
@@ -1,272 +1,265 @@
 import React from "react";
 import styled from "styled-components";
 import ContactInfoItem from "./ContactInfoItem";
-import { MdSchool } from 'react-icons/md';
-import { AiOutlineCheckCircle } from 'react-icons/ai';
-import { FaUserAlt } from 'react-icons/fa';
-import { FaRegFileAlt } from 'react-icons/fa';
-import { AiOutlineBarChart } from 'react-icons/ai';
-import { FaDollarSign } from 'react-icons/fa';
-import { FaRegCreditCard } from 'react-icons/fa';
-import { FaRocket } from 'react-icons/fa';
-
-
+import { MdSchool } from "react-icons/md";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { FaUserAlt } from "react-icons/fa";
+import { FaRegFileAlt } from "react-icons/fa";
+import { AiOutlineBarChart } from "react-icons/ai";
+import { FaDollarSign } from "react-icons/fa";
+import { FaRegCreditCard } from "react-icons/fa";
+import { FaRocket } from "react-icons/fa";
 
 const ContactBannerStyles = styled.div`
-    padding: 10rem 0;
-    .contactBanner__wrapper {
-        background-color: var(--deep-dark);
-        border-radius: 12px;
-        padding: 5rem 0rem;
-        text-align: center;
-        box-shadow: 17px 11px 23px #000;
+  padding: 10rem 0;
+  .contactBanner__wrapper {
+    background-color: var(--deep-dark);
+    border-radius: 12px;
+    padding: 5rem 0rem;
+    text-align: center;
+    box-shadow: 17px 11px 23px #000;
+  }
+  .right,
+  .right__right,
+  .left__left {
+    flex: 2;
+    .para {
+      font-size: 1.3rem;
     }
-    .right,
-    .right__right,
+  }
+  .about__subheading__up {
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+    font-size: 2.5rem;
+    text-align: center;
+  }
+
+  @media only screen and (max-width: 1170px) {
+    .top-section {
+      gap: 1rem;
+    }
+
     .left__left {
-        flex: 2;
-        .para {
-            font-size: 1.3rem;
-        }
-        
-        
+      padding: 2rem 0;
+      flex: 3;
+      .para {
+        font-size: 1rem;
+      }
     }
-        .about__subheading__up {
-        margin-top: 3rem;
-        margin-bottom: 3rem;
-        font-size: 2.5rem;
-         text-align: center;
-        
-       
+  }
+  @media only screen and (max-width: 1030px) {
+    padding: 10rem 0;
+    .top-section {
+      gap: 1rem;
     }
-    
-    
 
-    
-
-    @media only screen and (max-width: 1170px) {
-        
-        .top-section {
-            gap: 1rem;
-        }
-        
-        .left__left {
-            padding: 2rem 0;
-            flex: 3;
-            .para {
-            font-size: 1rem;
-        }
-        }
+    .aboutSection__left {
+      flex: 4;
     }
-    @media only screen and (max-width: 1030px) {
-        padding: 10rem 0;
-        .top-section {
-            gap: 1rem;
-        }
-        
-        .aboutSection__left {
-            flex: 4;
-        }
-        .left__left {
-        padding: 2rem 0;
-            flex: 3;
-        }
-        .left__left,
-        .right__right {
-            max-width: 100%;
-            width: 100%;
-            
-            .para {
-                font-size: 1rem;
-           }
-            
-        }
-        .text {
-        font-weight: 1rem;
-        }
-        .icon {
-        color: var(--white);
-        background-color: var(--gray-2);
-        padding: 1.3rem;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
+    .left__left {
+      padding: 2rem 0;
+      flex: 3;
     }
-     svg {
-         width: 3rem;
-        }
-            
+    .left__left,
+    .right__right {
+      max-width: 100%;
+      width: 100%;
+
+      .para {
+        font-size: 1rem;
+      }
     }
-    
-    @media only screen and (max-width: 931px) {
-        padding: 10rem 0;
-        .top-section {
-            flex-direction: column;
-            gap: 0rem;
-        }
-        .about__subheading {
-            font-size: 1.8rem;
-        }
-        .about__heading {
-            font-size: 2.8rem;
-        }
-        .about__info__heading {
-            font-size: 3rem;
-        }
-          .contactSection__wrapper {
-            flex-direction: column;
-        }
-        .contactSection__wrapper::after {
-            display: none
-        }
-        
-        .left__left,
-        .right__right {
-            max-width: 100%;
-            padding: 4rem 2rem 2rem 2rem;
-           .para {
-                font-size: 2rem;
-           }
-            
-        }
-            
-        .about__subheading__up {
-            text-align: center;
-        }
+    .text {
+      font-weight: 1rem;
+    }
+    .icon {
+      color: var(--white);
+      background-color: var(--gray-2);
+      padding: 1.3rem;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+    }
+    svg {
+      width: 3rem;
+    }
+  }
 
-        .icon {
-        color: var(--white);
-        background-color: var(--gray-2);
-        padding: 1.3rem;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        }
-        svg {
-        width: 4rem;
-        }
-}
-         @media only screen and (max-width: 450px) {
-         .left__left,
-        .right__right {
-            max-width: 100%;
-            padding: 4rem 2rem 2rem 2rem;
-           .para {
-                font-size: 1.5rem;
-           }
-            
-        }
+  @media only screen and (max-width: 931px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 0rem;
+    }
+    .about__subheading {
+      font-size: 1.8rem;
+    }
+    .about__heading {
+      font-size: 2.8rem;
+    }
+    .about__info__heading {
+      font-size: 3rem;
+    }
+    .contactSection__wrapper {
+      flex-direction: column;
+    }
+    .contactSection__wrapper::after {
+      display: none;
+    }
 
-        .icon {
-        color: var(--white);
-        background-color: var(--gray-2);
-        padding: 1.3rem;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        }
-        svg {
-        width: 3rem;
-        }
+    .left__left,
+    .right__right {
+      max-width: 100%;
+      padding: 1rem 1rem 1rem 1rem;
+      .para {
+        font-size: 2rem;
+      }
+    }
 
+    .about__subheading__up {
+      text-align: center;
+    }
 
-         }
+    .icon {
+      color: var(--white);
+      background-color: var(--gray-2);
+      padding: 1.3rem;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+    }
+    svg {
+      width: 4rem;
+    }
+  }
+  @media only screen and (max-width: 450px) {
+    .left__left,
+    .right__right {
+      max-width: 100%;
+      padding: 4rem 2rem 2rem 2rem;
+      .para {
+        font-size: 1.5rem;
+      }
+    }
 
-          @media only screen and (max-width: 361px) {
-         .left__left,
-        .right__right {
-            max-width: 100%;
-            padding: 4rem 2rem 2rem 2rem;
-           .para {
-                font-size: 1rem;
-           }
-            
-        }
+    .icon {
+      color: var(--white);
+      background-color: var(--gray-2);
+      padding: 1.3rem;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+    }
+    svg {
+      width: 3rem;
+    }
+  }
 
-        .icon {
-        color: var(--white);
-        background-color: var(--gray-2);
-        padding: 1.3rem;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        }
-        svg {
-        width: 2.5rem;
-        }
+  @media only screen and (max-width: 361px) {
+    .left__left,
+    .right__right {
+      max-width: 100%;
+      padding: 4rem 2rem 2rem 2rem;
+      .para {
+        font-size: 1rem;
+      }
+    }
 
+    .icon {
+      color: var(--white);
+      background-color: var(--gray-2);
+      padding: 1.3rem;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+    }
+    svg {
+      width: 2.5rem;
+    }
+  }
+  @media only screen and (max-width: 300px) {
+    .left__left,
+    .right__right {
+      max-width: 100%;
+      padding: 4rem 2rem 2rem 2rem;
+      .para {
+        font-size: 0.7rem;
+      }
+    }
 
-         }
-         @media only screen and (max-width: 300px) {
-         .left__left,
-        .right__right {
-            max-width: 100%;
-            padding: 4rem 2rem 2rem 2rem;
-           .para {
-                font-size: 0.7rem;
-           }
-            
-        }
-
-        .icon {
-        color: var(--white);
-        background-color: var(--gray-2);
-        padding: 1.3rem;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        }
-        svg {
-        width: 2.5rem;
-        }
-
-
-         }
-    
+    .icon {
+      color: var(--white);
+      background-color: var(--gray-2);
+      padding: 1.3rem;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+    }
+    svg {
+      width: 2.5rem;
+    }
+  }
 `;
 
 export default function ContactBanner() {
-    return (
-        <ContactBannerStyles>
-            <div className="contactBanner__wrapper">
-            <div className="container">
-                
-                    <p className="about__subheading__up">
-                                         <span>COMPROMISO, EXCELENCIA Y RESULTADOS</span>
-                                    </p>
-                                    <div className="top-section">
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<MdSchool />} text="ESPECIALIZACIÓN Y EXPERIENCIA" />
-                                        </div>
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<AiOutlineCheckCircle  />} text="COMPROBAMOS VIABILIDAD"/>
-                                        </div>
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<FaUserAlt  />} text="ATENCIÓN PERSONALIZADA"/>
-                                        </div>
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<FaRocket  />} text="PROCESOS ÁGILES"/>
-                                        </div>
-                                    </div>
-                                    <div className="top-section">
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<FaRegFileAlt    />} text="MÚLTIPLES CASOS ANUALES"/>
-                                        </div>
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<AiOutlineBarChart  />} text="ALTO PORCIENTO ÉXITO"/>
-                                        </div>
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<FaDollarSign  />} text="HONORARIOS ECONÓMICOS"/>
-                                        </div>
-                                        <div className="left__left">
-                                                <ContactInfoItem icon={<FaRegCreditCard  />} text="PAGOS SIN INTERESES"/>
-                                        </div>
-                                        
-                                        
-                                    </div>
-                                    
-                </div>
+  return (
+    <ContactBannerStyles>
+      <div className="contactBanner__wrapper">
+        <div className="container">
+          <p className="about__subheading__up">
+            <span>COMPROMISO, EXCELENCIA Y RESULTADOS</span>
+          </p>
+          <div className="top-section">
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<MdSchool />}
+                text="ESPECIALIZACIÓN Y EXPERIENCIA"
+              />
             </div>
-        </ContactBannerStyles>
-    )
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<AiOutlineCheckCircle />}
+                text="COMPROBAMOS VIABILIDAD"
+              />
+            </div>
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<FaUserAlt />}
+                text="ATENCIÓN PERSONALIZADA"
+              />
+            </div>
+            <div className="left__left">
+              <ContactInfoItem icon={<FaRocket />} text="PROCESOS ÁGILES" />
+            </div>
+          </div>
+          <div className="top-section">
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<FaRegFileAlt />}
+                text="MÚLTIPLES CASOS ANUALES"
+              />
+            </div>
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<AiOutlineBarChart />}
+                text="ALTO PORCIENTO ÉXITO"
+              />
+            </div>
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<FaDollarSign />}
+                text="HONORARIOS ECONÓMICOS"
+              />
+            </div>
+            <div className="left__left">
+              <ContactInfoItem
+                icon={<FaRegCreditCard />}
+                text="PAGOS SIN INTERESES"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </ContactBannerStyles>
+  );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,122 +1,123 @@
-
 import React from "react";
 import PText from "../components/PText";
-import AboutImg from "../assets/images/about-page-img.png"
+import AboutImg from "../assets/images/about-page-img.png";
 import styled from "styled-components";
 import ContactBanner from "../components/ContactBanner";
 import Advantages from "../components/Advantages";
 
 const AboutPageStyles = styled.div`
-    padding: 20rem 0 10rem 0;
+  padding: 20rem 0 10rem 0;
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--deep-dark);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--gray-1);
+      border-radius: 2rem;
+      box-shadow: 17px 11px 23px #000;
+    }
+  }
+  .about__info__items {
+    margin-top: 15rem;
+  }
+  .about__info__item {
+    margin-botton: 10rem;
+  }
+  .about__info__heading {
+    font-size: 3.6rem;
+    text-transform: uppercase;
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
     .top-section {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 2rem;
-    }
-    .left {
-        flex: 3;
-    }
-    .right {
-        flex: 2;
+      flex-direction: column;
+      gap: 5rem;
     }
     .about__subheading {
-        font-size: 2.2rem;
-        margin-bottom: 2rem;
-        span {
-            background-color: var(--deep-dark);
-            padding: 0.5rem;
-            border-radius: 8px;
-        }
+      font-size: 1.8rem;
     }
     .about__heading {
-        font-size: 3.6rem;
-        margin-bottom: 3rem;
-    }
-    .about__info {
-        margin-bottom: 4rem;
-        .para {
-            max-width: 100%;
-        }
-    }
-    .right {
-        img {
-            border: 2px solid var(--gray-1);
-            border-radius: 2rem;
-            box-shadow: 17px 11px 23px #000;
-        }
-    }
-    .about__info__items {
-        margin-top: 15rem;
-    }
-    .about__info__item {
-        margin-botton: 10rem;
+      font-size: 2.8rem;
     }
     .about__info__heading {
-        font-size: 3.6rem;
-        text-transform: uppercase;
+      font-size: 3rem;
     }
-    @media only screen and (max-width: 768px) {
-        padding: 10rem 0;
-        .top-section {
-            flex-direction: column;
-            gap: 5rem;
-        }
-        .about__subheading {
-            font-size: 1.8rem;
-        }
-        .about__heading {
-            font-size: 2.8rem;
-        }
-        .about__info__heading {
-            font-size: 3rem;
-        }
-    }
-
+  }
 `;
 
 export default function About() {
-    return (
-        <AboutPageStyles>
-            <div className="container">
-                <div className="top-section">
-                    <div className="left">
-                        <p className="about__subheading">
-                            HOLA, SOY <span>GABRIEL RODRIGUEZ</span>
-                        </p>
-                        <h2 className="about__heading">DOCTOR EN JURISPRUDENCIA</h2>
-                        <div className="about__info">
-                            <PText>
-                            COMO ABOGADO, MI PRINCIPAL COMPROMISO ES DEFENDER LOS DERECHOS 
-                            DE MIS CLIENTES CON INTEGRIDAD Y PROFESIONALISMO.
-                            CADA CASO REPRESENTA UN RETO ÚNICO QUE REQUIERE UN ANÁLISIS 
-                            DETALLADO Y ESTRATEGIAS PERSONALIZADAS PARA GARANTIZAR LA MEJOR 
-                            REPRESENTACIÓN LEGAL POSIBLE
-                            <br /> <br />
-                            CUENTO CON UNA AMPLIA EXPERIENCIA EN DIVERSAS ÁREAS DEL DERECHO, 
-                            INCLUYENDO DERECHO CIVIL, PENAL, LABORAL, MERCANTIL, FAMILIAR Y 
-                            ADMINISTRATIVO. ESTA TRAYECTORIA ME PERMITE ABORDAR CADA CASO CON 
-                            UNA VISIÓN INTEGRAL Y PROPONER SOLUCIONES EFECTIVAS ADAPTADAS A LAS NECESIDADES DE MIS CLIENTES
-                            <br /> <br />
-                            EL EJERCICIO DEL DERECHO EXIGE NO SOLO CONOCIMIENTO PROFUNDO 
-                            DE LAS NORMATIVAS VIGENTES, SINO TAMBIÉN LA CAPACIDAD DE NEGOCIAR Y ARGUMENTAR CON SOLIDEZ.
-                            EN CADA SITUACIÓN, BUSCO SOLUCIONES EFICIENTES Y FAVORABLES, 
-                            PRIORIZANDO SIEMPRE LOS INTERESES Y LA TRANQUILIDAD DE MIS REPRESENTADOS
-                            <br /> <br />
-                            EN UN MUNDO EN CONSTANTE CAMBIO, LA ACTUALIZACIÓN JURÍDICA ES FUNDAMENTAL.
-                            POR ELLO, ME MANTENGO AL DÍA CON LAS ÚLTIMAS REFORMAS LEGALES Y PRECEDENTES 
-                            JUDICIALES, ASEGURANDO QUE MIS CLIENTES RECIBAN ASESORÍA BASADA EN LA LEGISLACIÓN MÁS RECIENTE Y EFECTIVA
-                            </PText>
-                        </div>
-                       
-                    </div>
-                    <div className="right">
-                        <img src={AboutImg} alt="Gabriel Rodriguez img"/>
-                    </div>
-                </div>
-                <Advantages />
+  return (
+    <AboutPageStyles>
+      <div className="container">
+        <div className="top-section">
+          <div className="left">
+            <p className="about__subheading">
+              HOLA, SOY <span>GABRIEL RODRIGUEZ</span>
+            </p>
+            <h2 className="about__heading">DOCTOR EN JURISPRUDENCIA</h2>
+            <div className="about__info">
+              <PText>
+                COMO ABOGADO, MI PRINCIPAL COMPROMISO ES DEFENDER LOS DERECHOS
+                DE MIS CLIENTES CON INTEGRIDAD Y PROFESIONALISMO. CADA CASO
+                REPRESENTA UN RETO ÚNICO QUE REQUIERE UN ANÁLISIS DETALLADO Y
+                ESTRATEGIAS PERSONALIZADAS PARA GARANTIZAR LA MEJOR
+                REPRESENTACIÓN LEGAL POSIBLE
+                <br /> <br />
+                CUENTO CON UNA AMPLIA EXPERIENCIA EN DIVERSAS ÁREAS DEL DERECHO,
+                INCLUYENDO DERECHO CIVIL, PENAL, LABORAL, MERCANTIL, FAMILIAR Y
+                ADMINISTRATIVO. ESTA TRAYECTORIA ME PERMITE ABORDAR CADA CASO
+                CON UNA VISIÓN INTEGRAL Y PROPONER SOLUCIONES EFECTIVAS
+                ADAPTADAS A LAS NECESIDADES DE MIS CLIENTES
+                <br /> <br />
+                EL EJERCICIO DEL DERECHO EXIGE NO SOLO CONOCIMIENTO PROFUNDO DE
+                LAS NORMATIVAS VIGENTES, SINO TAMBIÉN LA CAPACIDAD DE NEGOCIAR Y
+                ARGUMENTAR CON SOLIDEZ. EN CADA SITUACIÓN, BUSCO SOLUCIONES
+                EFICIENTES Y FAVORABLES, PRIORIZANDO SIEMPRE LOS INTERESES Y LA
+                TRANQUILIDAD DE MIS REPRESENTADOS
+                <br /> <br />
+                EN UN MUNDO EN CONSTANTE CAMBIO, LA ACTUALIZACIÓN JURÍDICA ES
+                FUNDAMENTAL. POR ELLO, ME MANTENGO AL DÍA CON LAS ÚLTIMAS
+                REFORMAS LEGALES Y PRECEDENTES JUDICIALES, ASEGURANDO QUE MIS
+                CLIENTES RECIBAN ASESORÍA BASADA EN LA LEGISLACIÓN MÁS RECIENTE
+                Y EFECTIVA
+              </PText>
             </div>
-            <ContactBanner />
-        </AboutPageStyles>
-    );
+          </div>
+          <div className="right">
+            <img src={AboutImg} alt="Gabriel Rodriguez img" />
+          </div>
+        </div>
+        <Advantages />
+      </div>
+      <ContactBanner />
+    </AboutPageStyles>
+  );
 }

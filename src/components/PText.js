4c27f5d76b4b69f3c886938fedc4f473641fa063
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-01-21 22:05:57 -0500

    Actualizacion

--- src/components/PText.js
+++ src/components/PText.js
@@ -4,10 +4,8 @@ import styled from "styled-components";
 const PStyle = styled.div`
   max-width: 500px;
   margin: 0 auto;
-  font-size: 1.5rem;
+  font-size: 1.7rem;
   line-height: 2.5rem;
-  word-break: break-all;
-  overflow-wrap: break-word;
   @media only screen and (max-width: 768px) {
     font-size: 1.4rem;
   }

7467424e4e0e2124a78b11dc50a7ab303c3027fd
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-01-31 21:44:45 -0500

    corrigiendo ContactBanner

--- src/components/ContactBanner.js
+++ src/components/ContactBanner.js
@@ -1,39 +1,38 @@
 import React from "react";
-import PText from "./PText"; 
-import Button from "./Button"; 
+import PText from "./PText";
+import Button from "./Button";
 import styled from "styled-components";
 
 const ContactBannerStyles = styled.div`
-    padding: 10rem 0;
-    .contactBanner__wrapper {
-        background-color: var(--deep-dark);
-        border-radius: 12px;
-        padding: 5rem 0rem;
-        text-align: center;
-        box-shadow: 17px 11px 23px #000;
-    }
+  padding: 10rem 0;
+  .contactBanner__wrapper {
+    background-color: #1e293b;
+    border-radius: 12px;
+    padding: 5rem 0rem;
+    text-align: center;
+    box-shadow: 6px 6px 6px #000;
+  }
+  .contactBanner__heading {
+    font-size: 4rem;
+    margin-bottom: 2rem;
+  }
+  @media only screen and (max-width: 768px) {
     .contactBanner__heading {
-        font-size: 4rem;
-        margin-bottom: 2rem;
+      font-size: 2.8rem;
     }
-    @media only screen and (max-width: 768px) {
-        .contactBanner__heading {
-            font-size: 2.8rem;
-        }
-    }
-    
+  }
 `;
 
 export default function ContactBanner() {
-    return (
-        <ContactBannerStyles>
-            <div className="container">
-                <div className="contactBanner__wrapper">
-                    <PText>TIENES ALGUNA PREGUNTA</PText>
-                    <h3 className="contactBanner__heading">DEJAME AYUDARTE</h3>
-                    <Button btnText="CONTACTAME AHORA" btnLink="/contact"/>
-                </div>
-            </div>
-        </ContactBannerStyles>
-    )
+  return (
+    <ContactBannerStyles>
+      <div className="container">
+        <div className="contactBanner__wrapper">
+          <PText>TIENES ALGUNA PREGUNTA</PText>
+          <h3 className="contactBanner__heading">DEJAME AYUDARTE</h3>
+          <Button btnText="CONTACTAME AHORA" btnLink="/contact" />
+        </div>
+      </div>
+    </ContactBannerStyles>
+  );
 }

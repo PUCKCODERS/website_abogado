2ea480f65880af2e2d471e9adffe6d68bf46675e
PUCKCODERS <jonitaweb.jobs@gmail.com>
2026-02-14 10:40:17 -0500

    Post edicion

--- src/pages/ConsultaLegal.js
+++ src/pages/ConsultaLegal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Button from "../components/Button";
+
 import ContactBanner from "../components/ContactBanner";
 import {
   MdPhone,
